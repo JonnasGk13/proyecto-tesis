2d18e2eef8c46e632bd206da67a0c47a5717bf08
JonnasGk13 <j.ramirez.g1@edu.utc.mx>
2022-03-11 01:29:19 -0600

    ADD: funcionamiento de carrito de compras.

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,22 @@
 
 let $containerAnuncios = document.querySelector('main'); //guardamos mi clase "anuncio" en containerAnuncios
 let containerCompra = document.querySelector('.contenido'); 
+let priceTotal = document.querySelector('.price-total');
+let amountProduct = document.querySelector('.count-product');
+
+
+let comprarE = [];
+let Total = 0; 
+let countProduct = 0;
+
 
 //functions
 
 //agregamos un evento click a mis anuncios(cartas) y le pasamos la funcion addproduct
 function EventListener(){
     $containerAnuncios.addEventListener('click', agregarProducto);
+
+    containerCompra.addEventListener('click', borrarProducto); 
  }
  EventListener();
 
@@ -20,6 +30,22 @@ function agregarProducto(e){
         contenido(productoS)
     } 
  }
+ function borrarProducto(e){
+     if (e.target.classList.contains('eliminar-producto')){
+         const eliminarId = e.target.getAttribute('data.id')
+         comprarE.forEach(value => {
+             if(value.id == deleteId){
+                 let priceReduce =parseFloat(value.precio)*parseFloat(value.amount);
+                 Total =Total-priceReduce;
+                 Total = Total.toFixed(2);
+             }
+         })
+         comprarE = comprarE.filter(productoI => productoI.id !== deleteId); 
+         countProduct --;
+     }
+     cargaHTML();
+      
+ }
 
  function contenido(producto){
      const productoD = {
@@ -31,11 +57,30 @@ function agregarProducto(e){
          contador: 1
      }
 
+     Total = parseFloat (Total)  + parseFloat(infoproduct.precio);
+     Total = Total.toFixed(2);
+
+     const exist = comprarE.some(productoI => productoI.id === infoproduct.id);
+     if(exist){
+         const prod =comprarE.map(producto => {
+             if(producto.id === infoproduct.id){
+                 producto.agregarProducto++;
+                 return producto;
+                }else{
+                    return producto
+                }
+             });
+            agregarProducto= [...prod];
+        }else{
+            comprarE = [...comprarE,infoproduct]
+            countProduct ++;
+        }
+     }
      comprarE = [...comprarE, productoI]
      cargaHTML();
      console.log(productoI);
      //console.log(productoD); 
- }
+     ;
 
 
  function cargaHTML(){
@@ -53,8 +98,17 @@ function agregarProducto(e){
         <h6>${contador}</h6>
         </div>
         <span class="eliminarO">X</span>
-        `
+        `;
+        containerCompra.agregarProducto(r);
+
+        precio.innerHTML = Total;
+
+        amountProduct.innerHTML = countProduct;
      });
+function limpiarHTML(){
+        containerCompra.innerHTML = '';
+
+}
  }
 
  
